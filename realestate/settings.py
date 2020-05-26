2607ecc800f766ddbeed1de27eca8d54f42812f8
JoshuaMonyei <monyeijosh01@gmail.com>
2020-05-26 14:44:25 +0100

    Update settings.py

--- realestate/settings.py
+++ realestate/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'staticfiles')
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
